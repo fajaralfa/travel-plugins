2c079f673c82c072ef7df2a2a41b1d03dbc7d906
fajaralfa <fajaralfrzi@gmail.com>
2024-10-21 18:38:12 +0700

    boilerplate one-at-a-time plugin

--- one-at-a-time/src/main/java/id/co/itasoft/fajaralfa/budgetreq/OneAtATimeTool.java
+++ one-at-a-time/src/main/java/id/co/itasoft/fajaralfa/budgetreq/OneAtATimeTool.java
@@ -1,4 +1,42 @@
 package id.co.itasoft.fajaralfa.budgetreq;
 
-public class OneAtATimeTool {
+import org.joget.plugin.base.DefaultApplicationPlugin;
+
+import java.util.Map;
+
+public class OneAtATimeTool extends DefaultApplicationPlugin {
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public String getVersion() {
+        return "";
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
+
+    @Override
+    public Object execute(Map map) {
+        return null;
+    }
+
+    @Override
+    public String getLabel() {
+        return "";
+    }
+
+    @Override
+    public String getClassName() {
+        return "";
+    }
+
+    @Override
+    public String getPropertyOptions() {
+        return "";
+    }
 }
